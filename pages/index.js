566075a19413b15a836b04ea552cd411ca57c046
Denver Edwards <csdedwards@gmail.com>
2021-11-14 21:34:55 -0500

    MINOR CHANGE

--- pages/index.js
+++ pages/index.js
@@ -62,7 +62,7 @@ export default function Home() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8">
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 text-sm">
       <form onSubmit={handleSubmit}>
         <div>
           <label
