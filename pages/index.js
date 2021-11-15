6a5c74d8af1bafef43362a4f360d569dac47f22f
Denver Edwards <csdedwards@gmail.com>
2021-11-14 21:19:27 -0500

    minor changes
    padding change

--- pages/index.js
+++ pages/index.js
@@ -62,7 +62,7 @@ export default function Home() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen p-8">
       <form onSubmit={handleSubmit}>
         <div>
           <label
